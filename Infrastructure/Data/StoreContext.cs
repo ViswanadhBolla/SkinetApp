2134b1204da86da5a6059c58d0a7c8bf163f8779
ViswanadhBolla <viswanadth1234@gmail.com>
2022-10-03 22:01:17 +0530

    Add project files.

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Core.Models;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
